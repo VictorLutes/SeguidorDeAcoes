0d7b2de90e3d25ad1946d47f450a32a4489a5cab
VictorLutes <victorlutes@usp.br>
2022-11-20 11:58:51 -0300

    added variables to hide passwords

--- followShares/followShares/settings.py
+++ followShares/followShares/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from config import secretKey, emailHostPassword, emailHostUser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c#x4!v^22al$@cbuw#8vx&#%nhr-zeiaotl$a3g21)f=e-4x0u'
+SECRET_KEY = secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,8 +41,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'victorclutes@gmail.com'
-EMAIL_HOST_PASSWORD = 'tllsvdscgahekjqm'
+EMAIL_HOST_USER = emailHostUser
+EMAIL_HOST_PASSWORD = emailHostPassword
 
 
 INSTALLED_APPS = [
